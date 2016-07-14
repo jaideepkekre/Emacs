80816e6c9cd11e1ef483c63c0c6da8b631b07c92
Jaideep Kekre <jaideepkekre@users.noreply.github.com>
2016-07-14 23:39:47 +0530

    Update custom.el

--- custom.el
+++ custom.el
@@ -36,7 +36,8 @@
 (defun my/python-mode-hook ()
   (add-to-list 'company-backends 'company-jedi))
 
-
+;;(eval-after-load "company"
+;;'(add-to-list 'company-backends '(company-jedi :with company-capf)))
 (setq company-frontends
       '(company-pseudo-tooltip-unless-just-one-frontend
         company-preview-frontend
